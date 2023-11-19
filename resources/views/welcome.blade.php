87a269340fd7cc41979c3e7bc75c4bda0ba60c33
Welen1911 <welenalmeida2210@gmail.com>
2023-11-19 10:25:15 -0300

    Feat: auth with github

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,1 @@
+<a href="{{ route('socialite.login') }}">Login with Github</a>
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/logged', function () {
+    dd(auth()->user());
+});
+
+Route::get('/auth/github/redirect', function () {
+    return Socialite::driver('github')->redirect();
+})->name('socialite.login');
+
+Route::get('/auth/github/callback', function () {
+    $githubUser = Socialite::driver('github')->user();
+
+    $user = User::updateOrCreate([
+        'github_id' => $githubUser->id,
+    ], [
+        'name' => $githubUser->nickname,
+        'email' => $githubUser->email,
+        'github_token' => $githubUser->token,
+        'github_refresh_token' => $githubUser->refreshToken,
+    ]);
+
+    Auth::login($user);
+    return redirect('/logged');
+    // $user->token
+});
